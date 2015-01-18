9f5c7a7abc4d7d6f50ba597abc0a2de0a90462ba
Tarang Khanna <tarangalbert@gmail.com>
2015-01-18 12:01:53 -0500

    refresh data

--- raft-0.01/modules/youtube.js
+++ raft-0.01/modules/youtube.js
@@ -5,19 +5,22 @@
 	var me = {};
 
 	me = function() {
-		raft.register_callable("Youtube.convert", me.Youtubify);
+		raft.register_callable("Youtube.embedvid", me.embedvid);
 	}
-    var myId = ; //get from text
-        if (match && match[2].length == 11) {
+    var myId = watch?v=zbYf5_S7oJo; //get from text
+    
+    if (match && match[2].length == 11) {
         return match[2];
     } else {
-        return 'error';
-    }
-	me.Youtubify = function(s) {
-		return s.replace('UUWW', '<iframe width="560" height="315" src="//www.youtube.com/embed/' 
+    
+    } 
+  
+	me.embedvid = function(s) {
+		return s.replace('UUYOUTUBEWW', '<iframe width="560" height="315" src="//www.youtube.com/embed/' 
     + myId + '" frameborder="0" allowfullscreen></iframe>');
 	}
 	//[[]]
 	return me;
 
 }(raft))();
+
